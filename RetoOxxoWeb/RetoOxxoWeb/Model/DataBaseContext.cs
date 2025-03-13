7a525d586ab1c694801cbd114c9dfcdf370f45cc
EzAndre3 <A00838689@tec.mx>
2025-03-12 19:54:05 -0600

    Conexion con base de datos y modificaciones en index

---
+++ RetoOxxoWeb/RetoOxxoWeb/Model/DataBaseContext.cs
@@ -0,0 +1,147 @@
+using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace RetoOxxoWeb.Model
+{
+    public class DataBaseContext
+    {
+        public string ConnectionString {get; set;}
+        public DataBaseContext()
+        {
+            ConnectionString = "Server=127.0.0.1;Port=3306;Database=oxxojuego;Uid=root;password=Andre2005;";
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public usuario GetUsuarioEncima(int idUsuarioActual)
+        {
+            usuario usuarioEncima = null;
+
+            using (MySqlConnection conexion = GetConnection())
+            {
+                conexion.Open();
+                string queryArriba = @"
+                    SELECT * FROM (
+                        SELECT 
+                            u.id_usuario, 
+                            u.nombre, 
+                            AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3 AS promedio_puntos
+                        FROM 
+                            usuario u
+                        LEFT JOIN 
+                            taberna t ON u.id_usuario = t.id_usuario
+                        LEFT JOIN 
+                            laberinto l ON u.id_usuario = l.id_usuario
+                        LEFT JOIN 
+                            decision d ON u.id_usuario = d.id_usuario
+                        GROUP BY 
+                            u.id_usuario
+                        ORDER BY 
+                            promedio_puntos DESC
+                    ) ranking
+                    WHERE promedio_puntos < (
+                        SELECT AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3
+                        FROM 
+                            usuario u
+                        LEFT JOIN 
+                            taberna t ON u.id_usuario = t.id_usuario
+                        LEFT JOIN 
+                            laberinto l ON u.id_usuario = l.id_usuario
+                        LEFT JOIN 
+                            decision d ON u.id_usuario = d.id_usuario
+                        WHERE u.id_usuario = 4
+                    )
+                    ORDER BY promedio_puntos DESC
+                    LIMIT 1;";
+
+                MySqlCommand cmd = new MySqlCommand(queryArriba, conexion);
+                cmd.Parameters.AddWithValue("4", idUsuarioActual);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        usuarioEncima = new usuario
+                        {
+                            id_usuario = Convert.ToInt32(reader["id_usuario"]),
+                            nombre = reader["nombre"].ToString()
+                        };
+                    }
+                }
+            }
+
+            return usuarioEncima;
+        }
+
+        public usuario GetUsuarioDebajo(int idUsuarioActual)
+        {
+            usuario usuarioDebajo = null;
+
+            using (MySqlConnection conexion = GetConnection())
+            {
+                conexion.Open();
+                string queryAbajo = @"
+                    SELECT * FROM (
+                        SELECT 
+                            u.id_usuario, 
+                            u.nombre, 
+                            AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3 AS promedio_puntos
+                        FROM 
+                            usuario u
+                        LEFT JOIN 
+                            taberna t ON u.id_usuario = t.id_usuario
+                        LEFT JOIN 
+                            laberinto l ON u.id_usuario = l.id_usuario
+                        LEFT JOIN 
+                            decision d ON u.id_usuario = d.id_usuario
+                        GROUP BY 
+                            u.id_usuario
+                        ORDER BY 
+                            promedio_puntos DESC
+                    ) ranking
+                    WHERE promedio_puntos > (
+                        SELECT AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3
+                        FROM 
+                            usuario u
+                        LEFT JOIN 
+                            taberna t ON u.id_usuario = t.id_usuario
+                        LEFT JOIN 
+                            laberinto l ON u.id_usuario = l.id_usuario
+                        LEFT JOIN 
+                            decision d ON u.id_usuario = d.id_usuario
+                        WHERE u.id_usuario = 4
+                    )
+                    ORDER BY promedio_puntos ASC
+                    LIMIT 1;";
+
+                MySqlCommand cmd = new MySqlCommand(queryAbajo, conexion);
+                cmd.Parameters.AddWithValue("4", idUsuarioActual);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        usuarioDebajo = new usuario
+                        {
+                            id_usuario = Convert.ToInt32(reader["id_usuario"]),
+                            nombre = reader["nombre"].ToString()
+                        };
+                    }
+                }
+            }
+
+            return usuarioDebajo;
+        }
+
+
+
+
+        
+
+
+    }
+}
--- RetoOxxoWeb/RetoOxxoWeb/Pages/Index.cshtml.cs
+++ RetoOxxoWeb/RetoOxxoWeb/Pages/Index.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RetoOxxoWeb.Model;
 
 namespace RetoOxxoWeb.Pages;
 
+
 public class IndexModel : PageModel
 {
+    private readonly DataBaseContext _context;
+    public usuario UsuarioEncima {get;set;}
+    public usuario UsuarioDebajo {get;set;}
+    public int ProgresoTaberna { get; set; }
+    public int ProgresoLaberinto { get; set; }
+    public int ProgresoDecision { get; set; }
+
+    public IndexModel(DataBaseContext context)
+    {
+        _context = context;
+    }
+
     public void OnGet()
     {
+        UsuarioEncima = _context.GetUsuarioEncima(4);
+        UsuarioDebajo = _context.GetUsuarioDebajo(4);
 
     }
 }
