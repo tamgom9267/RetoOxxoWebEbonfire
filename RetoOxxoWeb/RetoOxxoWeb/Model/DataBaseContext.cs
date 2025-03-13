b517ad3e6d556e17530018ccf2b89d4007d74830
EzAndre3 <A00838689@tec.mx>
2025-03-12 21:50:18 -0600

    Index Completamente Funcional
    Todo el index esta conectado a la BD y con el ID de Laura como fijo

--- RetoOxxoWeb/RetoOxxoWeb/Model/DataBaseContext.cs
+++ RetoOxxoWeb/RetoOxxoWeb/Model/DataBaseContext.cs
@@ -29,7 +29,7 @@ namespace RetoOxxoWeb.Model
                         SELECT 
                             u.id_usuario, 
                             u.nombre, 
-                            AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3 AS promedio_puntos
+                            AVG(IFNULL(t.puntos,0) + IFNULL(l.puntos,0) + IFNULL(d.puntos,0)) / 3 AS promedio_puntos
                         FROM 
                             usuario u
                         LEFT JOIN 
@@ -44,7 +44,7 @@ namespace RetoOxxoWeb.Model
                             promedio_puntos DESC
                     ) ranking
                     WHERE promedio_puntos < (
-                        SELECT AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3
+                        SELECT AVG(IFNULL(t.puntos,0) + IFNULL(l.puntos,0) + IFNULL(d.puntos,0)) / 3
                         FROM 
                             usuario u
                         LEFT JOIN 
@@ -89,7 +89,7 @@ namespace RetoOxxoWeb.Model
                         SELECT 
                             u.id_usuario, 
                             u.nombre, 
-                            AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3 AS promedio_puntos
+                            AVG(IFNULL(t.puntos,0) + IFNULL(l.puntos,0) + IFNULL(d.puntos,0)) / 3 AS promedio_puntos
                         FROM 
                             usuario u
                         LEFT JOIN 
@@ -104,7 +104,7 @@ namespace RetoOxxoWeb.Model
                             promedio_puntos DESC
                     ) ranking
                     WHERE promedio_puntos > (
-                        SELECT AVG(IFNULL(t.puntos_g1,0) + IFNULL(l.puntos_g3,0) + IFNULL(d.puntos_j2,0)) / 3
+                        SELECT AVG(IFNULL(t.puntos,0) + IFNULL(l.puntos,0) + IFNULL(d.puntos,0)) / 3
                         FROM 
                             usuario u
                         LEFT JOIN 
@@ -137,10 +137,119 @@ namespace RetoOxxoWeb.Model
             return usuarioDebajo;
         }
 
+        public (int ProgresoTaberna, int ProgresoLaberinto, int ProgresoDecision) GetProgresoUsuario(int idUsuarioActual)
+        {
+            int progresoTaberna = 0;
+            int progresoLaberinto = 0;
+            int progresoDecision = 0;
 
+            using (MySqlConnection conexion = GetConnection())
+            {
+                conexion.Open();
 
+                string queryProgreso = @"
+                    SELECT 
+                        IFNULL(t.puntos, 0) AS progreso_taberna,
+                        IFNULL(l.puntos, 0) AS progreso_laberinto,
+                        IFNULL(d.puntos, 0) AS progreso_decision
+                    FROM 
+                        usuario u
+                    LEFT JOIN 
+                        taberna t ON u.id_usuario = t.id_usuario
+                    LEFT JOIN 
+                        laberinto l ON u.id_usuario = l.id_usuario
+                    LEFT JOIN 
+                        decision d ON u.id_usuario = d.id_usuario
+                    WHERE 
+                        u.id_usuario = 4;";
 
-        
+                MySqlCommand cmd = new MySqlCommand(queryProgreso, conexion);
+                cmd.Parameters.AddWithValue("4", idUsuarioActual);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        progresoTaberna = Convert.ToInt32(reader["progreso_taberna"]);
+                        progresoLaberinto = Convert.ToInt32(reader["progreso_laberinto"]);
+                        progresoDecision = Convert.ToInt32(reader["progreso_decision"]);
+                    }
+                }
+            }
+            int porcentajeTaberna = (progresoTaberna * 100) / 500;
+            int porcentajeLaberinto = (progresoLaberinto * 100) / 300;
+            int porcentajeDecision = (progresoDecision * 100) / 200;
+
+            return (porcentajeTaberna, porcentajeLaberinto, porcentajeDecision);
+        }
+
+        public (int FoodService, int EjecucionPromociones, int EquiposCompletos, int Rotacion, int FaltanteEfectivo) GetMetricasDeTienda(int idUsuario)
+        {
+            int foodService = 0;
+            int ejecucionPromociones = 0;
+            int equiposCompletos = 0;
+            int rotacion = 0;
+            int faltanteEfectivo = 0;
+
+            using (MySqlConnection conexion = GetConnection())
+            {
+                conexion.Open();
+
+                string queryMetricas = @"
+                    SELECT 
+                        food_service,
+                        ejecucion_promociones,
+                        equipos_completos,
+                        rotacion,
+                        faltante_efectivo
+                    FROM 
+                        metricas_de_tienda
+                    WHERE 
+                        id_usuario = 4;";
+
+                MySqlCommand cmd = new MySqlCommand(queryMetricas, conexion);
+                cmd.Parameters.AddWithValue("4", idUsuario);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        foodService = Convert.ToInt32(reader["food_service"]);
+                        ejecucionPromociones = Convert.ToInt32(reader["ejecucion_promociones"]);
+                        equiposCompletos = Convert.ToInt32(reader["equipos_completos"]);
+                        rotacion = Convert.ToInt32(reader["rotacion"]);
+                        faltanteEfectivo = Convert.ToInt32(reader["faltante_efectivo"]);
+                    }
+                }
+            }
+
+            return (foodService, ejecucionPromociones, equiposCompletos, rotacion, faltanteEfectivo);
+        }
+
+        public string GetNombreUsuario(int idUsuario)
+        {
+            string nombreUsuario = string.Empty;
+
+            using (MySqlConnection conexion = GetConnection())
+            {
+                conexion.Open();
+
+                string queryNombre = "SELECT nombre FROM usuario WHERE id_usuario = 4;";
+                MySqlCommand cmd = new MySqlCommand(queryNombre, conexion);
+                cmd.Parameters.AddWithValue("4", idUsuario);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        nombreUsuario = reader["nombre"].ToString();
+                    }
+                }
+            }
+
+            return nombreUsuario;
+        }
+
 
 
     }
--- RetoOxxoWeb/RetoOxxoWeb/Pages/Index.cshtml.cs
+++ RetoOxxoWeb/RetoOxxoWeb/Pages/Index.cshtml.cs
@@ -13,6 +13,11 @@ public class IndexModel : PageModel
     public int ProgresoTaberna { get; set; }
     public int ProgresoLaberinto { get; set; }
     public int ProgresoDecision { get; set; }
+    public int FoodService { get; set; }
+    public int EjecucionPromociones { get; set; }
+    public int EquiposCompletos { get; set; }
+    public int Rotacion { get; set; }
+    public int FaltanteEfectivo { get; set; }
 
     public IndexModel(DataBaseContext context)
     {
@@ -21,8 +26,18 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        var metricas = _context.GetMetricasDeTienda(4);
+        var (taberna, laberinto, decision) = _context.GetProgresoUsuario(4);
         UsuarioEncima = _context.GetUsuarioEncima(4);
         UsuarioDebajo = _context.GetUsuarioDebajo(4);
+        ProgresoTaberna = taberna;
+        ProgresoLaberinto = laberinto;
+        ProgresoDecision = decision;
+        FoodService = metricas.FoodService;
+        EjecucionPromociones = metricas.EjecucionPromociones;
+        EquiposCompletos = metricas.EquiposCompletos;
+        Rotacion = metricas.Rotacion;
+        FaltanteEfectivo = metricas.FaltanteEfectivo;
 
     }
 }
