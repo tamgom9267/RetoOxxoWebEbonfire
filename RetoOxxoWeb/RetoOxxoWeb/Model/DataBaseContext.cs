c3bbbb8dc577f34a69a9e024466c957a0416bd01
A01723266 <A01723266@tec.mx>
2025-03-12 23:05:07 -0600

    Update DataBaseContext.cs

--- RetoOxxoWeb/RetoOxxoWeb/Model/DataBaseContext.cs
+++ RetoOxxoWeb/RetoOxxoWeb/Model/DataBaseContext.cs
@@ -9,7 +9,7 @@ namespace RetoOxxoWeb.Model
         public string ConnectionString {get; set;}
         public DataBaseContext()
         {
-            ConnectionString = "Server=127.0.0.1;Port=3306;Database=oxxojuego;Uid=root;password=3314;";
+            ConnectionString = "Server=127.0.0.1;Port=3306;Database=oxxojuego;Uid=root;password=root;";
         }
 
         private MySqlConnection GetConnection()
