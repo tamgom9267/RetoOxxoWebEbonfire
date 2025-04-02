acc7da4dea6059a251027277fd8d074c9c37f881
Santiago Tamez <stamez26@icloud.com>
2025-04-01 19:05:27 -0600

    La tabla de clasifiación general ahora usa datos de la bd

---
+++ RetoOxxoWeb/RetoOxxoWeb/Model/Laberinto.cs
@@ -0,0 +1,13 @@
+using System;
+using Org.BouncyCastle.Asn1.Cms;
+
+namespace RetoOxxoWeb.Model
+{
+    public class laberinto
+    {
+        public int id_laberinto {get; set;}
+        public int puntos {get; set;}
+        public Time ?tiempo {get; set;}
+        public int id_usuario {get; set;}
+    }
+}
