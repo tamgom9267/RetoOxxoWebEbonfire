2fb4e1b11a3e77a236c6e1ee5e099359453016ce
Andre Hernandez <A00838689@tec.mx>
2025-04-28 13:02:06 -0600

    Pagina juego correcta

--- RetoOxxoWeb/RetoOxxoWeb/Pages/Progreso_Capacitacion.cshtml.cs
+++ RetoOxxoWeb/RetoOxxoWeb/Pages/Progreso_Capacitacion.cshtml.cs
@@ -6,8 +6,10 @@ namespace RetoOxxoWeb.Pages;
 
 public class Progreso_CapacitacionModel : PageModel
 {
+    public string userFoto { get; set; }
 
     public void OnGet()
     {
+        userFoto = HttpContext.Session.GetString("userFoto");
     }
 }
